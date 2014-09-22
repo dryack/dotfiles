a529da9518d0d026cef755d7d1dfb5188105416e
dryack <dave.ryack@urbanairship.com>
2014-09-22 15:14:23 -0700

    ....

--- prep.sh
+++ prep.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # TOOD: Add target to .ssh/config
 TARGET=$1
-infocmp screen-256color | ssh ${TARGET} "cat - > screen-256color"
-ssh ${TARGET} "tic screen-256color && rm screen-256color"
-ssh -A ${TARGET}
+infocmp screen-256color | /usr/bin/ssh ${TARGET} "cat - > screen-256color && tic screen-256color && rm screen-256color"
+cat ~/projects/dotfiles/.bashrc | sed s/ssh=\'prep.sh\'/ssh='ssh -A'/ | /usr/bin/ssh ${TARGET} "cat - > .bashrc"
+/usr/bin/ssh -A ${TARGET}
