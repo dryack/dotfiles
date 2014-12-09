3c60ed7bc1152810f0bf0a96c58c7cd7f375fb54
dryack <dave.ryack@urbanairship.com>
2014-12-09 12:48:38 -0800

    Fixed prompt for dealing with non-local machines.

--- prep.sh
+++ prep.sh
@@ -2,5 +2,5 @@
 # TOOD: Add target to .ssh/config
 TARGET=$1
 infocmp screen-256color | /usr/bin/ssh ${TARGET} "cat - > screen-256color && tic screen-256color && rm screen-256color"
-cat ~/projects/dotfiles/.bashrc | sed s/ssh=\'prep.sh\'/ssh="'ssh -A'"/ | sed -e '/syspip(){/{N;N;d;}' | sed -e '/mac2unix()/{N;N;d;}' | sed /export/d | sed /2kad/d | /usr/bin/ssh ${TARGET} "cat - > .bashrc"
+cat ~/projects/dotfiles/.bashrc | sed s/ssh=\'prep.sh\'/ssh="'ssh -A'"/ | sed s/~..rvm.bin.rvm-prompt/hostname/ | sed -e '/syspip(){/{N;N;d;}' | sed -e '/mac2unix()/{N;N;d;}' | sed /export/d | sed /2kad/d | /usr/bin/ssh ${TARGET} "cat - > .bashrc"
 /usr/bin/ssh -A ${TARGET}
