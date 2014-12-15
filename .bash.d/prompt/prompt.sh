10f63e30afbec7dce8907bca61db62c3ea477a3c
dryack <dave.ryack@urbanairship.com>
2014-12-15 14:46:55 -0800

    gah

--- .bash.d/prompt/prompt.sh
+++ .bash.d/prompt/prompt.sh
@@ -2,7 +2,7 @@
 
 if [[ "$OSTYPE" =~ ^darwin ]]; then
     source $BASHD/completions/git-prompt.sh
-    PS1='|\A| '$PS_Red'($(~/.rvm/bin/rvm-prompt))$PS_NC'\n \w/'$PS_Cyan'$(__git_ps1 " (%s)")'$PS_NC' \$ '
+    PS1='|\A| '$PS_Red'($(~/.rvm/bin/rvm-prompt))'$PS_NC'\n \w/'$PS_Cyan'$(__git_ps1 " (%s)")'$PS_NC' \$ '
 else
     PS1='|\A| '$PS_Red'($PS1HOST)'$PS_NC' '$PS_Cyan'[\u]'$PS_NC' $ '
 fi
