8e8d071c858302d526ed522d396221ce97b144d0
dryack <dave.ryack@urbanairship.com>
2014-12-15 14:40:55 -0800

    meh

--- .bash.d/env.sh
+++ .bash.d/env.sh
@@ -8,13 +8,13 @@ export TERM=xterm-256color
 
 
 if [[ "$OSTYPE" =~ ^linux ]]; then
-    PS1HOST="$(hostname)"
+    PS1HOST='$(hostname)'
 fi
 
 if [[ "$OSTYPE" =~ ^darwin ]]; then
     GIT_PS1_SHOWDIRTYSTATE=1
     GIT_PS1_SHOWSTASHSTATE=1
-    RVMPROMPT="$(~/.rvm/bin/rvm-prompt)"
+    RVMPROMPT='$(~/.rvm/bin/rvm-prompt)'
     export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
     export HOMEBREW_GITHUB_API_TOKEN=5797cdf376cf9417b080465fd707950ca06177d1
     export PIP_REQUIRE_VIRTUALENV=true
