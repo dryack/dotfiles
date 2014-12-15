3e1f2096202eddc8a3f44840d7ead530b9c6059f
dryack <dave.ryack@urbanairship.com>
2014-12-15 14:44:23 -0800

    further mehs

--- .bash.d/env.sh
+++ .bash.d/env.sh
@@ -14,7 +14,6 @@ fi
 if [[ "$OSTYPE" =~ ^darwin ]]; then
     GIT_PS1_SHOWDIRTYSTATE=1
     GIT_PS1_SHOWSTASHSTATE=1
-    RVMPROMPT='$(~/.rvm/bin/rvm-prompt)'
     export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
     export HOMEBREW_GITHUB_API_TOKEN=5797cdf376cf9417b080465fd707950ca06177d1
     export PIP_REQUIRE_VIRTUALENV=true
