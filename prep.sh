d0d1fd0c22dd4525a6e2729989a1339753154031
dryack <dave.ryack@urbanairship.com>
2014-12-16 15:32:43 -0800

    rsync for the win.
    only update servers that nead it.

--- prep.sh
+++ prep.sh
@@ -2,7 +2,6 @@
 # TOOD: Add target to .ssh/config
 TARGET=$1
 infocmp screen-256color | /usr/bin/ssh ${TARGET} "cat - > screen-256color && tic screen-256color && rm screen-256color;mkdir -p .bash.d"
-#cat ~/.bashrc | sed -e '/GIT_PS1/{N;N;N;d;}' | sed /2wait/d | sed -e '/syspip(){/{N;N;N;N;N;d;}' | sed /export/d | sed /2kad/d | gsed '/PS1/c\'PS1="\'\\\A (\$(hostname)) [\\\u] $ \'" | /usr/bin/ssh ${TARGET} "cat - > .bashrc"
-scp -rq ~/.bash.d dryack@${TARGET}:~
-cat ~/.bashrc | /usr/bin/ssh ${TARGET} "cat - > .bashrc"
+rsync -vuz ~/.bashrc dryack@${TARGET}:~/
+rsync -vurz ~/.bash.d dryack@${TARGET}:~/
 /usr/bin/ssh -A ${TARGET}
