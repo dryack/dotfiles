5604106a5c544acd085ec081bd31ff7bc6c470f9
dryack <dave.ryack@urbanairship.com>
2014-12-16 09:35:23 -0800

    brain cancer

--- .bash.d/prompt/prompt.sh
+++ .bash.d/prompt/prompt.sh
@@ -4,5 +4,5 @@ if [[ "$OSTYPE" =~ ^darwin ]]; then
     source $BASHD/completions/git-prompt.sh
     PS1='|\A| '$PS_Red'($(~/.rvm/bin/rvm-prompt))'$PS_NC'\n \w/'$PS_Cyan'$(__git_ps1 " (%s)")'$PS_NC' \$ '
 else
-    PS1='|\A| '$PS_Red'($(localhost))'$PS_NC' '$PS_Cyan'[\u]'$PS_NC' $ '
+    PS1='|\A| '$PS_Red'($(hostname))'$PS_NC' '$PS_Cyan'[\u]'$PS_NC' $ '
 fi
