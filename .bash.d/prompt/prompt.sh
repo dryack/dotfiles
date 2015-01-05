dce4291db7f68613396dd70d484b84be25d359c2
dryack <dave.ryack@urbanairship.com>
2015-01-05 07:42:48 -0800

    More prevention of partial commandline retention

--- .bash.d/prompt/prompt.sh
+++ .bash.d/prompt/prompt.sh
@@ -2,7 +2,7 @@
 
 if [[ "$OSTYPE" =~ ^darwin ]]; then
     source $BASHD/completions/git-prompt.sh
-    PS1='|\A| '$PS_Red'($(~/.rvm/bin/rvm-prompt))'$PS_NC'\n \w/'$PS_Cyan'$(__git_ps1 " (%s)")'$PS_NC' \$ '
+    PS1='\n|\A| '$PS_Red'($(~/.rvm/bin/rvm-prompt))'$PS_NC' '$PS_Cyan'$(__git_ps1 " (%s)")'$PS_NC'\n\w/ \$ '
 else
-    PS1='|\A| '$PS_Red'($(hostname))'$PS_NC' '$PS_Cyan'[\u]'$PS_NC' \$ '
+    PS1='\n|\A| '$PS_Red'($(hostname))'$PS_NC' '$PS_Cyan'[\u]'$PS_NC' \w/ \$ '
 fi
