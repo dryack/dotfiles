85a1d3f69c7890cd4c5a56790c309225b7e76619
dryack <dave.ryack@urbanairship.com>
2015-01-28 07:47:42 -0800

    Additional refinements.

--- .bash.d/env.sh
+++ .bash.d/env.sh
@@ -16,6 +16,6 @@ if [[ "$OSTYPE" =~ ^darwin ]]; then
     GIT_PS1_SHOWSTASHSTATE=1
     export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
     export HOMEBREW_GITHUB_API_TOKEN=5797cdf376cf9417b080465fd707950ca06177d1
-    export PIP_REQUIRE_VIRTUALENV=true
+    #export PIP_REQUIRE_VIRTUALENV=true
     export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
 fi
