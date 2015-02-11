ee2c48b79a7603e684761c45c5a209f2b19b4fe7
dryack <dave.ryack@urbanairship.com>
2015-02-11 13:56:21 -0800

    Don't need non-quiet rsync anymore.

--- prep.sh
+++ prep.sh
@@ -2,6 +2,6 @@
 # TOOD: Add target to .ssh/config
 TARGET=$1
 infocmp screen-256color | /usr/bin/ssh ${TARGET} "cat - > screen-256color && tic screen-256color && rm screen-256color;mkdir -p .bash.d"
-rsync -vuz ~/.bashrc dryack@${TARGET}:~/
-rsync -vurz ~/.bash.d dryack@${TARGET}:~/
+rsync -vuzq ~/.bashrc dryack@${TARGET}:~/
+rsync -vurzq ~/.bash.d dryack@${TARGET}:~/
 /usr/bin/ssh -A ${TARGET}
