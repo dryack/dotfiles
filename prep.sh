f12a2834654badf169fefab84428637d2bb3aa82
dryack <dave.ryack@urbanairship.com>
2015-06-02 08:49:53 -0700

    Added .vimrc to prep

--- prep.sh
+++ prep.sh
@@ -2,6 +2,7 @@
 # TOOD: Add target to .ssh/config
 TARGET=$1
 infocmp screen-256color | /usr/bin/ssh ${TARGET} "cat - > screen-256color && tic screen-256color && rm screen-256color;mkdir -p .bash.d"
+rsync -vuzq ~/.vimrc dryack@${TARGET}:~/
 rsync -vuzq ~/.bashrc dryack@${TARGET}:~/
 rsync -vurzq ~/.bash.d dryack@${TARGET}:~/
 /usr/bin/ssh -A ${TARGET}
