69994eeb6b70132fbf4646583a76eb075bf569d9
dryack <dave.ryack@urbanairship.com>
2016-02-01 10:56:11 -0800

    GO Support

--- .bash.d/paths.sh
+++ .bash.d/paths.sh
@@ -1,7 +1,7 @@
 # Paths
 
 if [[ "$OSTYPE" =~ ^darwin ]]; then
-    PATH=$PATH:~/.rvm/bin:/usr/local/opt/ruby193/bin:/usr/local/lib/python2.7/site-packages:/usr/local/opt/qt5/bin:$PATH
+    PATH=$PATH:~/.rvm/bin:/usr/local/opt/ruby193/bin:/usr/local/lib/python2.7/site-packages:/usr/local/opt/qt5/bin:/usr/local/opt/go/libexec/bin:$PATH
 fi
 
 #if [[ "$OSTYPE" =~ ^linux ]]; then
