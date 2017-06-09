09c13cb418a0922102e5ec7d3d03ae067fd6f78a
dryack <dave.ryack@gmail.com>
2017-06-09 15:29:04 -0700

    Updated path for RVM

--- .bash.d/paths.sh
+++ .bash.d/paths.sh
@@ -1,8 +1,8 @@
 # Paths
 
 if [[ "$OSTYPE" =~ ^darwin ]]; then
-    PATH=$PATH:~/.rvm/bin:/usr/local/opt/ruby193/bin:/usr/local/lib/python2.7/site-packages:/usr/local/opt/qt5/bin:/usr/local/opt/go/libexec/bin:~/.cargo/bin:/usr/local/go/bin:$PATH
-    GOPATH=~/projects/go:~/projects/drac-kvm
+    PATH=$PATH:~/.rvm/bin:/usr/local/opt/ruby193/bin:/usr/local/lib/python2.7/site-packages:/usr/local/opt/qt5/bin:/usr/local/opt/go/libexec/bin:~/.cargo/bin:/usr/local/go/bin:~/.rvm/gems/ruby-2.4.1/bin:$PATH
+    GOPATH=~/projects/go
 fi
 
 #if [[ "$OSTYPE" =~ ^linux ]]; then
