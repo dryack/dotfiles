8253f633676a5e08e361f977b06a5a5b53fbfa26
dryack <dave.ryack@gmail.com>
2019-04-10 10:36:26 -0700

    testing terminal colors

--- .bash.d/functs.sh
+++ .bash.d/functs.sh
@@ -24,3 +24,20 @@ if [[ "$OSTYPE" =~ ^darwin ]]; then
     complete -F _complete_ssh_hosts ping
 fi
 ## /ssh autocomplete
+
+test_term_colors()
+{
+awk 'BEGIN{
+    s="/\\/\\/\\/\\/\\"; s=s s s s s s s s;
+    for (colnum = 0; colnum<77; colnum++) {
+        r = 255-(colnum*255/76);
+        g = (colnum*510/76);
+        b = (colnum*255/76);
+        if (g>255) g = 510-g;
+        printf "\033[48;2;%d;%d;%dm", r,g,b;
+        printf "\033[38;2;%d;%d;%dm", 255-r,255-g,255-b;
+        printf "%s\033[0m", substr(s,colnum+1,1);
+    }
+    printf "\n";
+}'
+}
