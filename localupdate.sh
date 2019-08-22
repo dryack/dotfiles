d9e81d8e1696aa194a01ffc17695c0e25234a608
Dave Ryack <dave.ryack@gmail.com>
2019-08-22 08:34:57 -0700

    update prep.sh

--- localupdate.sh
+++ localupdate.sh
@@ -3,13 +3,13 @@ cd ~/projects/dryack/dotfiles
 blackbox_edit_start .bash.d/secrets-env.sh
 blackbox_edit_start .bash.d/secrets-aliases.sh
 blackbox_edit_start AIRSHIP.html
-cp -r ~/projects/dryack/dotfiles/.bash.d/ ~
+cp -R ~/projects/dryack/dotfiles/.bash.d/ ~/.bash.d
 cp ~/projects/dryack/dotfiles/fixkh.sh ~/scripts
 cp ~/projects/dryack/dotfiles/AIRSHIP.html ~
 cp ~/projects/dryack/dotfiles/.bashrc ~
 cp ~/projects/dryack/dotfiles/.bash_profile ~
 cp ~/projects/dryack/dotfiles/prep.sh ~/scripts
-cp ~/projects/dryacj/dotfiles/prep.sh ~/bin
+cp ~/projects/dryack/dotfiles/prep.sh ~/bin
 blackbox_edit_end .bash.d/secrets-env.sh
 blackbox_edit_end .bash.d/secrets-aliases.sh
 blackbox_edit_end AIRSHIP.html
