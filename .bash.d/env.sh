353559991c4a3166d27409e9ecb8d93905d23c84
Dave Ryack <dave.ryack@gmail.com>
2019-08-28 08:29:41 -0700

    pythonpath in osx

--- .bash.d/env.sh
+++ .bash.d/env.sh
@@ -39,6 +39,8 @@ if [[ "$OSTYPE" =~ ^darwin ]]; then
     eval $(ssh-agent)
     GIT_PS1_SHOWDIRTYSTATE=1
     GIT_PS1_SHOWSTASHSTATE=1
+    # mopidy
+    export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages:$PYTHONPATH
     # golang
     export PATH="$PATH:$HOME/go/bin"
     # candidate for blkbx
@@ -47,11 +49,7 @@ if [[ "$OSTYPE" =~ ^darwin ]]; then
     export HOMEBREW_GITHUB_API_TOKEN=79a4a291d02bbd6fbe417d6dc28a677b44484f94
     #export PIP_REQUIRE_VIRTUALENV=true
     export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
-    # for servo especially
     # candidate for blkbx
-    export PIP_INDEX_URL=https://packages.prod.urbanairship.com/pulp/python/web/pip/simple/
-    # candidate for blkbx
-    export PIP_TRUSTED_HOST=packages.prod.urbanairship.com
     ####
     export CPPFLAGS="-I/usr/local/opt/qt5/include"
     export LDFLAGS="-L/usr/local/opt/qt5/lib"
