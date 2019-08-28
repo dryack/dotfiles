29e5b689be68694c7f1362e41f591194655a4cc2
Dave Ryack <dave.ryack@gmail.com>
2019-08-28 10:12:18 -0700

    things

--- localupdate.sh
+++ localupdate.sh
@@ -3,6 +3,7 @@ cd ~/projects/dryack/dotfiles
 blackbox_edit_start .bash.d/secrets-env.sh
 blackbox_edit_start .bash.d/secrets-aliases.sh
 blackbox_edit_start AIRSHIP.html
+blackbox_edit_start tipsfile.txt
 cp -R ~/projects/dryack/dotfiles/.bash.d/ ~/.bash.d
 cp ~/projects/dryack/dotfiles/fixkh.sh ~/scripts
 cp ~/projects/dryack/dotfiles/AIRSHIP.html ~
@@ -10,8 +11,10 @@ cp ~/projects/dryack/dotfiles/.bashrc ~
 cp ~/projects/dryack/dotfiles/.bash_profile ~
 cp ~/projects/dryack/dotfiles/prep.sh ~/scripts
 cp ~/projects/dryack/dotfiles/prep.sh ~/bin
+cp ~/projects/dryack/dotfiles/tipsfile.txt ~
 blackbox_edit_end .bash.d/secrets-env.sh
 blackbox_edit_end .bash.d/secrets-aliases.sh
 blackbox_edit_end AIRSHIP.html
+blackbox_edit_end tipsfile.txt
 cd -
 echo "Remember to copy ~/scripts/* to ~/bin/"
