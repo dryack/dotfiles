a813faf5a9d21ccea1e35e15c752f78e99edbf8f
Dave Ryack <dave.ryack@gmail.com>
2019-10-07 10:25:47 -0700

    update localupdate.sh

--- localupdate.sh
+++ localupdate.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 cd ~/projects/dryack/dotfiles
-blackbox_edit_start .bash.d/secrets-env.sh
-blackbox_edit_start .bash.d/secrets-aliases.sh
-blackbox_edit_start AIRSHIP.html
-blackbox_edit_start tipsfile.txt
+blackbox_cat .bash.d/secrets-env.sh > ~/.bash.d/secrets-env.sh
+blackbox_cat .bash.d/secrets-aliases.sh > ~/.bash.d/secrets-aliases.sh
+blackbox_cat AIRSHIP.html > ~/AIRSHIP.html
+blackbox_cat tipsfile.txt > ~.tipsfile.txt
 cp -R ~/projects/dryack/dotfiles/.bash.d/ ~/.bash.d
 cp ~/projects/dryack/dotfiles/fixkh.sh ~/scripts
-cp ~/projects/dryack/dotfiles/AIRSHIP.html ~
 cp ~/projects/dryack/dotfiles/.bashrc ~
 cp ~/projects/dryack/dotfiles/.bash_profile ~
 cp ~/projects/dryack/dotfiles/prep.sh ~/scripts
 cp ~/projects/dryack/dotfiles/prep.sh ~/bin
-cp ~/projects/dryack/dotfiles/tipsfile.txt ~
 cp ~/projects/dryack/dotfiles/localupdate.sh ~/scripts
-blackbox_edit_end .bash.d/secrets-env.sh
-blackbox_edit_end .bash.d/secrets-aliases.sh
-blackbox_edit_end AIRSHIP.html
-blackbox_edit_end tipsfile.txt
 cd -
 echo "Remember to copy ~/scripts/* to ~/bin/"
